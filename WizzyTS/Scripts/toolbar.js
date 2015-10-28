92128ae28662c8e7e8b8326e68deabab9f7cb020
Taramouse <taramouse@live.co.uk>
2015-10-28 18:15:41 +0000

    Added event handlers. Although incomplete for buttons ready to add elements. Included a new experimients page with css3 animations, etc.

---
+++ WizzyTS/Scripts/toolbar.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    $("#addText").click(function () {
+        console.log("Add Text button clicked.")
+    });
+
+    $("#addImage").click(function () {
+        console.log("Add Image button clicked.")
+    });
+
+    $("#addFile").click(function () {
+        console.log("Add File button clicked.")
+    });
+})
